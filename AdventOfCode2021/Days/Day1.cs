82e7465dd3950e392d2483b31f4d5d4bb1876b9f
joacand <tbsitu@gmail.com>
2021-12-01 19:52:44 +0100

    Add solution for Day 1

--- AdventOfCode2021/Days/Day1.cs
+++ AdventOfCode2021/Days/Day1.cs
@@ -1,10 +1,39 @@
-﻿namespace AdventOfCode2021.Days
+﻿using AdventOfCode2021.Data;
+
+namespace AdventOfCode2021.Days
 {
     internal sealed class Day1 : IDay
     {
         public string Solve()
         {
-            return "Todo!";
+            var depths = EmbeddedResource.ReadInput("InputDay1.txt")
+                .Split('\n').Select(x => int.Parse(x)).ToArray();
+
+            var increases = CountDepthsIncreases(depths, 1).ToString();
+
+            var threeMeasurementSlidingIncreases = CountDepthsIncreases(depths, 3).ToString();
+
+            return $"Part 1: {increases}, Part 2: {threeMeasurementSlidingIncreases}";
+        }
+
+        private static int CountDepthsIncreases(int[] depths, int windowSize)
+        {
+            var increases = 0;
+            for (int i = 0; i + windowSize < depths.Length; i++)
+            {
+                var depthA = 0;
+                var depthB = 0;
+                for (int j = 0; j < windowSize; j++)
+                {
+                    depthA += depths[i + j];
+                    depthB += depths[i + j + 1];
+                }
+                if (depthB > depthA)
+                {
+                    increases++;
+                }
+            }
+            return increases;
         }
     }
 }

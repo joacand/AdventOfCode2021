f5682625165b9005f7ac51951afe43102d77fe80
joacand <tbsitu@gmail.com>
2021-12-07 17:03:42 +0100

    Add solution for Day 7

---
+++ AdventOfCode2021/Days/Day7.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode2021.Data;
+
+namespace AdventOfCode2021.Days
+{
+    internal sealed class Day7 : IDay
+    {
+        public string Solve()
+        {
+            var crabPositions = EmbeddedResource.ReadInput("InputDay7.txt").Split(',').Select(x => int.Parse(x.Trim()));
+
+            var part1Result = FindMinFuel(crabPositions, true);
+            var part2Result = FindMinFuel(crabPositions, false);
+
+            return $"Part 1: {part1Result}, Part 2: {part2Result}";
+        }
+
+        private static int FindMinFuel(IEnumerable<int> crabPositions, bool part1)
+        {
+            var minFuel = int.MaxValue;
+
+            for (var position = crabPositions.Min(); position <= crabPositions.Max(); position++)
+            {
+                var fuelCost = part1
+                    ? crabPositions.Sum(crabPosition => Math.Abs(crabPosition - position))
+                    : crabPositions.Sum(crabPosition => Math.Abs(crabPosition - position) * (Math.Abs(crabPosition - position) + 1) / 2);
+
+                minFuel = fuelCost < minFuel ? fuelCost : minFuel;
+            }
+
+            return minFuel;
+        }
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,6 +1,6 @@
 ﻿using AdventOfCode2021.Days;
 
-Day6 day = new();
+Day7 day = new();
 
 Console.WriteLine(day.Solve());
 Console.ReadLine();
